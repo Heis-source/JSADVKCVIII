074c4c6033669f1196de4514ac196a1cc1d9d738
Hodei Gomez Sundstrom <hodeigomz@gmail.com>
2020-01-26 20:58:02 +0100

    Subiendo proyecto

---
+++ _js/navbar.js
@@ -0,0 +1,21 @@
+function lStorage () {
+
+    const searchForm = document.getElementById('search-form');
+    const searchInput = document.getElementById('input.search');
+
+    searchForm.addEventListener('submit', evt => {
+        evt.preventDefault();
+        if (searchInput.validity.valid) {
+          // Pintar shows con el filtro!
+          firstLoad(searchInput.value);
+          // almacenar en localstorage o cookie storage
+          localStorage.setItem('last-search', searchInput.value);
+        }
+      });
+
+      if (localStorage.getItem('last-search')) {
+        firstLoad(localStorage.getItem('last-search'));
+      } else {
+        firstLoad();
+      }
+}
